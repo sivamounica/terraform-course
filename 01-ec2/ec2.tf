13984a9a1de2322b6b9855f3e3ae1abc750c92c9
MounicaRudra <503432936@ge.com>
2025-06-26 16:30:37 -0400

    git push origin main Updated

--- 01-ec2/ec2.tf
+++ 01-ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myec2" {
-   ami = "ami-04893cdb768d0f9ee"
+   ami = "ami-0c803b171269e2d72"
    instance_type = "t2.micro"
 
    tags = {
@@ -7,11 +7,3 @@ resource "aws_instance" "myec2" {
   }
 }
 
-resource "aws_instance" "myec2-1" {
-   ami = "ami-04893cdb768d0f9ee"
-   instance_type = "t2.micro"
-
-   tags = {
-    Name = "HelloWorld-1"
-  }
-}
--- 01-ec2/provider.tf
+++ 01-ec2/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.6.0"
+      version = "6.1.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-2"
 }
